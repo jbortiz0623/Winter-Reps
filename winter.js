b55af562c9aabd7a819cb32187e781fb18151b97
jbortiz0623 <jbortiz0623@gmail.com>
2019-01-02 07:54:50 -0800

    III. Arrays & Control flow G.

--- winter.js
+++ winter.js
@@ -248,3 +248,21 @@ if (number < 100) {
 else (number >= 100) {
   console.log("big number")
 }
+
+
+// Write an `if ... else if ... else` statement:
+
+// `console.log()` `little number` if the number entered is less than **5**.<br>
+
+// If the number entered is more than 10, log `big number`.
+
+// Otherwise, log "monkey". <br>
+
+if (number < 5) {
+  console.log("little number")
+}
+else if (number > 10) {
+  console.log("big number")
+} else {
+  console.log("monkey")
+}
