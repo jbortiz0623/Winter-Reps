0fd96ec00152a962e403d90bcbd0903cf84c75d7
jbortiz0623 <jbortiz0623@gmail.com>
2019-01-02 07:01:01 -0800

    first commit

---
+++ winter.js
@@ -0,0 +1,16 @@
+// How do we assign a value to a variable?
+var variable1 = value
+
+// How do we change the value of a variable?
+var variable1 = newValue
+
+// How do we assign an existing variable to a new variable?
+var variable2 = variable1
+
+// Remind me, what are 'declare', 'assign', and 'define'?
+// Declare meaning is to give a variable a name
+// Assign meaning is to give that variable a value
+// Define would be a combination of declare and assign.
+
+// What is pseudocoding and why should you do it?
+// pseudocoding explains a step by step process into planning, and its very useful becuase it helps figure out the approach and process.
