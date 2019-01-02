ebd23809978e905ca58f1213e9846bdb0ce2dbf2
jbortiz0623 <jbortiz0623@gmail.com>
2019-01-02 07:23:31 -0800

    II. Loops A.

--- winter.js
+++ winter.js
@@ -76,3 +76,20 @@ if (personAge >= 16) {
 else {
   console.log("sorry, youre too young")
 };
+
+// Write a loop that will print out all the numbers from 0 to 10, inclusive.
+for (let i=0; i < 11; i++) {
+  console.log(i);
+}
+
+// Write a loop that will print out all the numbers from 10 up to and including 400.
+for (let i=10; i <= 400; i++) {
+  console.log(i);
+}
+
+// Write a loop that will print out every third number starting with 12 and going no higher than 4000.
+for (let i=12; i <= 4000; i++) {
+  if (i % 3 == 0) {
+    console.log(i)
+  }
+}
