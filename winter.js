afa5c29565178176c77366b11ce90a35255dfa8a
jbortiz0623 <jbortiz0623@gmail.com>
2019-01-02 07:05:01 -0800

    strings

--- winter.js
+++ winter.js
@@ -14,3 +14,16 @@ var variable2 = variable1
 
 // What is pseudocoding and why should you do it?
 // pseudocoding explains a step by step process into planning, and its very useful becuase it helps figure out the approach and process.
+
+// Create a variable called 'firstVariable'.
+var firstVariable;
+// Assign it the value of the string ''"Hello World"''
+var firstVariable = 'Hello World';
+// Change the value of this variable to some number.
+firstVariable = 7;
+// Store the value of 'firstVariable' in a new variable called `secondVariable`
+var secondVariable = firstVariable
+// Change the value of 'secondVariable' to any string.
+secondVariable = "hello";
+// What is the value of 'firstVariable'?
+value of firstVariable is 7
