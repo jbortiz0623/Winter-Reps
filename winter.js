a5306dddd69d3508726c4d07545bec5f92b28934
jbortiz0623 <jbortiz0623@gmail.com>
2019-01-02 07:50:24 -0800

    III. Arrays & Control flow E.

--- winter.js
+++ winter.js
@@ -206,11 +206,29 @@ console.log(randomThings);
 
 // Given the following array `const ourClass = ["Salty", "Zoom", "Sardine", "Slack", "Github"]`
 const ourClass = ["Salty", "Zoom", "Sardine", "Slack", "Github"]
-// 1. What would you write to access the 3rd element of the array?
+// What would you write to access the 3rd element of the array?
 ourClass[2];
 
-// 2. Change the value of "Github" to "Octocat"
+// Change the value of "Github" to "Octocat"
 ourClass[4] = "Octocat";
 
-// 3. Add a new element, "Cloud City" to the array.
+// Add a new element, "Cloud City" to the array.
 ourclass.push("Cloud City");
+
+
+// Given the following array: `const myArray = [5, 10, 500, 20]`
+const myArray = [5, 10, 500, 20]
+// Add the string `"Egon"` to the end of the array. Add another string of your choice to the end of the array.
+myArray.push("Egon")
+myArray.push("hello")
+
+// Remove the `5` from the beginning of the array.
+myArray.shift()
+
+// Add the string `"Bob Marley"` to the beginning of the array.
+myArray.unshift("Bob Marley")
+
+// Remove the string of your choise from the end of the array.
+myArray.pop()
+// Reverse this array using `Array.prototype.reverse()`.  Did you mutate the array? What does _mutate_ mean? Did the `.reverse()` method return anything?
+myArray.reverse()
